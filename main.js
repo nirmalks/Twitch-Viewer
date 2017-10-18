9a8992887768b98a8569adec8fe6d2c8a40a97e0
Nirmal Kumar K S <kagosekar@gmail.com>
2017-10-18 21:41:07 +0530

    Added current stream status
    * Added current streaming status (either offline or stream content)
    * Separate call to get stream details

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
 function TwitchViewer() {
-    this.favChannels = ["ESL_SC2", "cretetion", "freecodecamp", "comster404"];
+    this.favChannels = ["ESL_SC2", "cretetion", "freecodecamp", "gamereactor"];
     this.favChannelsData = [];
-    this.twitchUrl = "https://wind-bow.glitch.me/twitch-api/channels/";
+    this.twitchUrl = "https://wind-bow.glitch.me/twitch-api/";
 }
 
 TwitchViewer.prototype.init = function () {
     this.getFavoriteChannels();
-    console.log(this.favChannelsData);
-
 };
 
 TwitchViewer.prototype.getFavoriteChannels = function () {
     let self = this;
     let promises = [];
+    let streamPromises = [];
     for (let i = 0; i < this.favChannels.length; i++) {
-        promises.push(fetch(this.twitchUrl + this.favChannels[i]).then(function (response) {
+        promises.push(fetch(this.twitchUrl + "channels/" + this.favChannels[i]).then(function (response) {
+            return response.json();
+        }));
+
+        streamPromises.push(fetch(this.twitchUrl + "streams/" + this.favChannels[i]).then(function (response) {
             return response.json();
         }));
     }
     Promise.all(promises)
         .then(function (data) {
             self.favChannelsData = data;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
+    Promise.all(streamPromises)
+        .then(function (data) {
+            for (let i = 0; i < self.favChannelsData.length; i++) {
+                self.favChannelsData[i].streamData = data[i];
+            }
+
             self.populateTable(self.favChannelsData);
         })
         .catch(function (err) {
@@ -29,12 +43,17 @@ TwitchViewer.prototype.getFavoriteChannels = function () {
 };
 
 TwitchViewer.prototype.populateTable = function (data) {
-    console.log(data);
     let target = $('tbody');
+    let channelStatus = "";
+
     for (let i = 0; i < data.length; i++) {
-        console.log(data[i]);
-        if(data[i].status !== 404) {
-            target.append(`<tr><td><img alt="${data[i].name}" src="${data[i].logo}"/> </td><td> ${data[i].name} </td><td> ${data[i].status} </td></tr>`);
+        if (data[i].status !== 404) {
+            if (data[i].streamData.stream !== null && data[i].streamData.stream !== undefined) {
+                channelStatus = data[i].status;
+            } else {
+                channelStatus = "offline";
+            }
+            target.append(`<tr><td><img width="75px" height="30px" alt="${data[i].name}" src="${data[i].logo}"/> </td><td> <a href="${data[i].url}">${data[i].name}</a> </td><td> ${channelStatus} </td></tr>`);
         }
     }
 };
